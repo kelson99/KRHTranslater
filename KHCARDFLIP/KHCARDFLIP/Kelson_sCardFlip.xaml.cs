3992826ef0ad0f5646e0da98eee5d3254619839f
Kelson Hartle <kelson99hartle@icloud.com>
2020-08-28 09:47:11 -0600

    COMPLETED PROJECT

---
+++ KHCARDFLIP/KHCARDFLIP/Kelson_sCardFlip.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+namespace KHCARDFLIP
+{
+    public partial class Kelson_sCardFlip : ContentPage
+    {
+        public Kelson_sCardFlip()
+        {
+            InitializeComponent();
+        }
+
+        void ButtonFlipCard_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Button selectedButton = (Button)sender;
+            if (selectedButton.Text == "Show Card Face")
+            {
+                ButtonFlipCard.Text = "Show Card Back";
+                AceFrontCardImage.IsVisible = true;
+                AceBackCardImage.IsVisible = false;
+            } else
+            {
+                ButtonFlipCard.Text = "Show Card Face";
+                AceBackCardImage.IsVisible = true;
+                AceFrontCardImage.IsVisible = false;
+            }
+        }
+    }
+}
---
+++ KHCARDFLIP/KHCARDFLIP/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KHCARDFLIP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void ClickToGoToPageTwoButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new Kelson_sCardFlip());
+        }
+    }
+}
